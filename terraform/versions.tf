48bd6d806a8c3fcf479c16b84d9fa00cd4144546
Jonathan Vansina <jonathan.vansina@inthepocket.com>
2022-02-16 17:25:25 +0100

    Add terraform block and providers

---
+++ terraform/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.10.0"
+    }
+  }
+}
+
+provider "google" {
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+  project = "workshop-terraform-hogent"
+}
