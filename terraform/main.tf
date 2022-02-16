740a71dc8e4bdb44d88ee5e372f01422c7747723
Jonathan Vansina <jonathan.vansina@inthepocket.com>
2022-02-16 17:23:48 +0100

    Add firewall and compute instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,37 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range = "10.216.${var.student_number}.0/24"
   network       = google_compute_network.vpc_network.id
 }
+
+# Firewall to allow http, https and SSH
+resource "google_compute_firewall" "default" {
+  name    = "itp-workshop-firewall-${var.student_number}"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["webserver"]
+}
+
+# Instance resource
+resource "google_compute_instance" "webserver" {
+  name         = "itp-workshop-webserver-${var.student_number}"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+
+    access_config {}
+  }
+
+  tags = ["webserver"]
+}
